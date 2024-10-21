5a1f1b8b7ab8861747075b2321779dd53fc630c4
falfiya <22552895+falfiya@users.noreply.github.com>
2024-10-21 09:57:13 -0700

    Finished Basics.v
    Moving on to Introduction.v

---
+++ coq/full/.Makefile.d
@@ -0,0 +1,6 @@
+Basics.vo Basics.glob Basics.v.beautified Basics.required_vo: Basics.v 
+Basics.vio: Basics.v 
+Basics.vos Basics.vok Basics.required_vos: Basics.v 
+Induction.vo Induction.glob Induction.v.beautified Induction.required_vo: Induction.v 
+Induction.vio: Induction.v 
+Induction.vos Induction.vok Induction.required_vos: Induction.v 
--- coq/full/Basics.v
+++ coq/full/Basics.v
@@ -1046,6 +1046,7 @@ Proof.
   rewrite H1.
   rewrite <- H2.
   reflexivity.
+  Qed.
 (** [] *)
 
 (** The [Admitted] command tells Coq that we want to skip trying
@@ -1523,11 +1524,19 @@ Inductive bin : Type :=
     for binary numbers, and a function [bin_to_nat] to convert
     binary numbers to unary numbers. *)
 
-Fixpoint incr (m:bin) : bin
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Fixpoint incr (m:bin) : bin :=
+  match m with
+  | Z => B1 Z
+  | B0 n => B1 n
+  | B1 n => B0 (incr n)
+  end.
 
-Fixpoint bin_to_nat (m:bin) : nat
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Fixpoint bin_to_nat (m:bin) : nat :=
+  match m with
+  | Z => O
+  | B0 n => 2 * bin_to_nat n
+  | B1 n => S (2 * bin_to_nat n)
+  end.
 
 (** The following "unit tests" of your increment and binary-to-unary
     functions should pass after you have defined those functions correctly.
@@ -1536,24 +1545,30 @@ Fixpoint bin_to_nat (m:bin) : nat
     next chapter. *)
 
 Example test_bin_incr1 : (incr (B1 Z)) = B0 (B1 Z).
-(* FILL IN HERE *) Admitted.
+  reflexivity.
+Qed.
 
 Example test_bin_incr2 : (incr (B0 (B1 Z))) = B1 (B1 Z).
-(* FILL IN HERE *) Admitted.
+  reflexivity.
+Qed.
 
 Example test_bin_incr3 : (incr (B1 (B1 Z))) = B0 (B0 (B1 Z)).
-(* FILL IN HERE *) Admitted.
+  reflexivity.
+Qed.
 
 Example test_bin_incr4 : bin_to_nat (B0 (B1 Z)) = 2.
-(* FILL IN HERE *) Admitted.
+  reflexivity.
+Qed.
 
 Example test_bin_incr5 :
         bin_to_nat (incr (B1 Z)) = 1 + bin_to_nat (B1 Z).
-(* FILL IN HERE *) Admitted.
+  reflexivity.
+Qed.
 
 Example test_bin_incr6 :
         bin_to_nat (incr (incr (B1 Z))) = 2 + bin_to_nat (B1 Z).
-(* FILL IN HERE *) Admitted.
+  reflexivity.
+Qed.
 
 (** [] *)
 
